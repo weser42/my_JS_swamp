4cfcf6b9810bc78ca0a9d02da51e448c75087b0b
weser42 <58133977+weser42@users.noreply.github.com>
2023-11-12 15:37:02 +0400

    add second day ex

--- tr_JS/app.js
+++ tr_JS/app.js
@@ -1,6 +1,62 @@
 "use strict"
-let a = 1;
-console.log(a);
-a ='hfhfhf'
-console.log(a);
-console.log('tththth');
+//let oneH=80;
+//let timePerDay = 5;
+//let timePerWeek = 5;
+//let timeLimit = 11;
+//let hoLim=40;
+//let projectTime = 8;
+//let workTime=1;
+//let week=["pn","wt","sr","ch","pt","sb","ws"];
+////console.log(week[1]);
+//let summTime= week.concat(week);
+////console.log(summTime[10]);
+////console.log(summTime[11]);
+
+//for (let i=0; i<=10; i++){
+//   //let workDay=[]
+//   //let workTime=1;
+   
+//   //workDay.push(summTime[i])
+//   if (summTime[i] !== summTime[5] && summTime[i] !== summTime[6] ) { console.log(` ${workTime = i ++}`)};  //workDay.push(summTime[i]);
+   
+   
+//   //workDay.push(summTime[i])
+
+////summTime[i];
+////console.log(workDay);
+////console.log(workTime*5);
+////console.log(summTime);
+
+//}
+//console.log(`${workTime*5>hoLim} + ${hoLim*oneH} `);
+// расчет процентов по депозиту
+
+//let orignDeposit = 12000;
+//let prDepositperYear = 7;
+//let prDepositperMonth = 7/12;
+//let onePersent;
+//let purpose = 13000;
+//for (let i = 0; i < 24; i++){
+//   let persentPerMonthNew = orignDeposit/100*prDepositperMonth;
+//let a = orignDeposit + persentPerMonthNew;
+//    orignDeposit = a;
+//   //let b = orignDeposit+newarr;
+//   console.log(`${i} : ${orignDeposit}`);
+//}
+//console.log(orignDeposit);
+// Проверка на робота
+//let a;
+//const res = prompt(`Сколько будет 7+-15?`)
+
+//switch(true){
+//   case res ==="iam not a Robot":
+//   case Number(res)===22:
+//   case Number(res)===-8:
+//      console.log('Sucsess');
+//      break;
+//   default:
+//      console.log('You are Robot');
+//}
+
+
+
